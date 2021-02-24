185e20b55157a7f32e4c5e04360924a891b4da65
JoieHuiju <joie.huiju@gmail.com>
2021-02-24 20:32:50 +0900

    [Add] Add part for user account
    - email을 사용한 가입, 로그인 구현
    - social 계정을 사용한 가입, 로그인 구현
    - 로그아웃 구현

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,95 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { firebaseInstance, authService } from 'fbase';
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState('');
+
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === 'email') {
+      setEmail(value);
+    } else {
+      setPassword(value);
+    }
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        // create account
+        data = await authService.createUserWithEmailAndPassword(
+          email,
+          password,
+        );
+      } else {
+        // sign in
+        data = await authService.signInWithEmailAndPassword(email, password);
+      }
+      console.log(data);
+    } catch (e) {
+      console.log(e);
+      setError(e.message);
+    }
+  };
+  const toggleNewAccount = () => setNewAccount((prev) => !prev);
+  const onSocialClick = async (event) => {
+    let {
+      target: { name },
+    } = event;
+    let provider;
+
+    if (name === 'google') {
+      // google socail login
+      provider = new firebaseInstance.auth.GoogleAuthProvider();
+    } else if (name === 'github') {
+      // github social login
+      provider = new firebaseInstance.auth.GithubAuthProvider();
+    }
+    const data = await authService.signInWithPopup(provider);
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={onChange}
+        />
+        <input type="submit" value={newAccount ? 'Create Account' : 'Log In'} />
+        {error}
+      </form>
+      <span onClick={toggleNewAccount}>
+        {newAccount ? 'Log In' : 'Create Account'}
+      </span>
+      <div>
+        <button name="google" onClick={onSocialClick}>
+          Continue with Google
+        </button>
+        <button name="github" onClick={onSocialClick}>
+          Continue with Github
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Auth;
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { authService } from 'fbase';
 
-const Profile = () => <span>Profile</span>;
+const Profile = () => {
+  let history = useHistory();
+  const onLogOutClick = () => {
+    authService.signOut();
+    history.push('/');
+  };
+
+  return <button onClick={onLogOutClick}>Log Out</button>;
+};
 
 export default Profile;
