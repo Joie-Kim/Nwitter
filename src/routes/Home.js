eb4a3ed7ae9f12114f8306bd0e9443af96510ff7
JoieHuiju <joie.huiju@gmail.com>
2021-02-24 21:54:55 +0900

    [Add] Add part for nweet
    - nweet(트윗과 같은 개념으로 사용)을 작성했을 때, firebase db에 저장
    - 저장된 nweet을 불러와서 화면에 보이도록 함
    - 실시간으로 db에 저장 및 읽어오기가 가능하도록 함

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,71 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { dbService } from 'fbase';
 
-const Home = () => <span>Home</span>;
+const Home = ({ userObj }) => {
+  const [nweet, setNweet] = useState('');
+  const [nweets, setNweets] = useState([]);
+
+  /*
+  const getNweets = async () => {
+    const dbNweets = await dbService.collection('nweets').get();
+    dbNweets.forEach((document) => {
+      const nweetObject = {
+        ...document.data(),
+        id: document.id,
+      };
+      setNweets((prev) => [nweetObject, ...prev]);
+    });
+  };
+  */
+  const onchange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    dbService.collection('nweets').add({
+      text: nweet,
+      createdAt: Date.now(),
+      createorId: userObj.uid,
+    });
+    setNweet('');
+  };
+
+  useEffect(() => {
+    //getNweets();
+
+    dbService.collection('nweets').onSnapshot((snapshot) => {
+      const nweetArray = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setNweets(nweetArray);
+    });
+  }, []);
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onchange}
+          value={nweet}
+          type="text"
+          placeholder="what's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Nweet" />
+      </form>
+      <div>
+        {nweets.map((nweet) => (
+          <div key={nweet.id}>
+            <h4>{nweet.text}</h4>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Home;
