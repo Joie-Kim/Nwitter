6b5f323c6b694a9031b28d4457cf512537f318a4
JoieHuiju <joie.huiju@gmail.com>
2021-02-26 21:55:22 +0900

    [Update] Change dynamically displayName of profile
    - 프로필 이름 변경할 때, Navigation에 있는 프로필 이름이 동적으로 변경되도록 수정

--- src/components/App.js
+++ src/components/App.js
@@ -10,21 +10,35 @@ function App() {
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if (user) {
-        //setIsLoggedIn(true);
-        setUserObj(user);
-      } /* else {
-        setIsLoggedIn(false);
-      }*/
+        setUserObj({
+          displayName: user.displayName,
+          uid: user.uid,
+          updateProfile: (args) => user.updateProfile(args),
+        });
+      }
       setInit(true);
     });
   }, []);
+  // firebase의 user에 변경사항이 있을 때, 다시 정보를 불러옴
+  const refreshUser = () => {
+    const user = authService.currentUser;
+    setUserObj({
+      displayName: user.displayName,
+      uid: user.uid,
+      updateProfile: (args) => user.updateProfile(args),
+    });
+  };
 
   // isLoggedIn 대신에 Boolean(userObj) 사용
   // 상태값 하나가 줄어들어서 render 줄어듦
   return (
     <>
       {init ? (
-        <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} />
+        <AppRouter
+          refreshUser={refreshUser}
+          isLoggedIn={Boolean(userObj)}
+          userObj={userObj}
+        />
       ) : (
         'Initializing ...'
       )}
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,15 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import { authService } from 'fbase';
+import { authService, dbService } from 'fbase';
 
-const Profile = () => {
-  let history = useHistory();
+const Profile = ({ userObj, refreshUser }) => {
+  const history = useHistory();
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
+
   const onLogOutClick = () => {
     authService.signOut();
     history.push('/');
   };
+  /*
+  const getMyNweets = async () => {
+    const nweets = await dbService
+      .collection('nweets')
+      .where('creatorId', '==', userObj.uid)
+      .orderBy('createdAt')
+      .get();
+    console.log(nweets);
+  };
+  */
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewDisplayName(value);
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    if (userObj.displayName !== newDisplayName) {
+      await userObj.updateProfile({
+        displayName: newDisplayName,
+      });
+    }
+    refreshUser();
+  };
 
-  return <button onClick={onLogOutClick}>Log Out</button>;
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          onChange={onChange}
+          placeholder="Diaplay Name"
+          value={newDisplayName}
+        />
+        <input type="submit" value="Update Profile" />
+      </form>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
 };
 
 export default Profile;
