504225e9a4a01cc315c6776f86c6eece8c16fca9
JoieHuiju <joie.huiju@gmail.com>
2021-03-19 23:58:59 +0900

    [Add] Upload profile picture
    - 프로필 사진 업로드를 위한 기능 추가
    - 저장 기능을 추가 해야 함

--- src/components/ProfileForm.js
+++ src/components/ProfileForm.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 const ProfileForm = ({ userObj, refreshUser }) => {
   const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
+  const [attachment, setAttachment] = useState('');
   const onChange = (event) => {
     const {
       target: { value },
@@ -17,8 +20,42 @@ const ProfileForm = ({ userObj, refreshUser }) => {
     }
     refreshUser();
   };
+  const onFileChange = (event) => {
+    const {
+      target: { files },
+    } = event;
+    const file = files[0];
+    const reader = new FileReader();
+    reader.onloadend = (finishedEvent) => {
+      const {
+        currentTarget: { result },
+      } = finishedEvent;
+      setAttachment(result);
+    };
+    if (Boolean(file)) {
+      reader.readAsDataURL(file);
+    }
+  };
+  const onClearAttachment = () => setAttachment('');
   return (
     <form onSubmit={onSubmit} className="profileForm">
+      {attachment && (
+        <div className="profilePhoto__attachment">
+          <img
+            src={attachment}
+            style={{
+              backgroundImage: attachment,
+            }}
+          />
+          <div
+            className="profilePhoto__label"
+            onClick={onClearAttachment}
+            style={{ marginBottom: 20 }}>
+            <span>Remove</span>
+            <FontAwesomeIcon icon={faTimes} />
+          </div>
+        </div>
+      )}
       <input
         type="text"
         onChange={onChange}
@@ -27,12 +64,28 @@ const ProfileForm = ({ userObj, refreshUser }) => {
         autoFocus
         className="formInput"
       />
+      <label
+        for="attach-file"
+        className="profilePhoto__label"
+        style={{ marginTop: 10 }}>
+        <span>Add photos</span>
+        <FontAwesomeIcon icon={faPlus} />
+      </label>
+      <input
+        id="attach-file"
+        type="file"
+        accept="image/*"
+        onChange={onFileChange}
+        style={{
+          opacity: 0,
+        }}
+      />
+
       <input
         type="submit"
         value="Update Profile"
         onClick={onSubmit}
         className="formBtn"
-        style={{ marginTop: 10 }}
       />
     </form>
   );
